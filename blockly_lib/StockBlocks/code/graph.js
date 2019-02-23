bbb5e56e9d221eef7ce93c7c574805e1ef9259a4
jgbakke <redwingjordan@gmail.com>
2019-02-23 11:24:42 -0500

    graph

---
+++ blockly_lib/StockBlocks/code/graph.js
@@ -0,0 +1,61 @@
+
+function writeGraph(timestamps, equities){
+
+    var borderColors = ["rgb(232, 75, 67)", "rgb(19, 107, 23)"];
+    var bgColors = ["rgb(209, 16, 6)", "rgb(55, 173, 61)"];
+
+    // 1 for Green, 0 for Red
+    var border = borderColors[+(equities[equities.length-1] > equities[0])];
+    var background = bgColors[+(equities[equities.length-1] > equities[0])];
+    
+    console.log(background);
+    console.log(border);
+
+
+    var ctx = document.getElementById('graph').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: timestamps,
+        datasets: [{
+            label: "Total Portfolio Value",
+            backgroundColor: background,
+            borderColor: border,
+            borderWidth:1,
+            lineTension:0,
+            data: equities,
+        }]
+    },
+
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 920000,
+                    max: 1080000
+                }
+            }],
+            
+            xAxes: [{
+                ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 20
+                }
+            }]
+        },
+        
+        elements : {
+            point: {
+                radius: 0
+            }
+        }
+    }
+    
+    });
+};
+
+
+writeGraph([...Array(150).keys()], Array.from({length: 150}, () => (Math.random() * 50000 + 1000000)));
